99ddabe12d14f9b83920939070e4674120225f85
Arnichex <92742665+Arnichex@users.noreply.github.com>
2024-02-15 08:16:35 +0000

    randomph

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ function App() {
   const[panala,setPanala]=useState(100);
 
   const generateRandomNumber = () => {
-    const randomNum = Math.floor(Math.random() * 100) + 1;
+    const randomNum = Math.floor(Math.random() * (parseInt(panala) - parseInt(dela) + 1)) + parseInt(dela);
     setRandomNumber(randomNum);
   };
 
   return (
     <div className='App'>
         <input placeholder='de la ' value={dela} onChange={(e)=>setDela(e.target.value)}></input>
-        <input placeholder='pana la ' value={panala} onChange={(e)=>setDela(e.target.value)}></input>
+        <input placeholder='pana la ' value={panala} onChange={(e)=>setPanala(e.target.value)}></input>
       <h1 className='titlu'>Random Number Generator</h1>
       <button onClick={generateRandomNumber}>Generate Random Number</button>
       {randomNumber !== null && <p>Generated Random Number: {randomNumber}</p>}
